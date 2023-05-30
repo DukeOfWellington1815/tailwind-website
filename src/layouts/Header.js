e83297aa0e20f51854ca1fcfb4dd9762865c8214
FatMonkee <89774607+FatMonkee@users.noreply.github.com>
2023-05-30 09:56:04 +0200

    header fixed
    thanks

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,17 +5,17 @@ import "../assets/styles/corporateDesign.css";
 export default function Header() {
   return (
     <nav className="bg-bright-color shadow border-0 p-4 flex sm:justify-center">
-      <div className="flex justify-between items-center">
-        <img src={logo} className="App-logo monkeylogo" alt="logo" />
+      <div className="w-full h-full flex justify-center items-center relative">
+        <img src={logo} className="App-logo monkeylogo  absolute left-0 top-1/2 -translate-y-1/2" alt="logo" />
         <div className="space-x-16">
           {[
             ["Home", "/dashboard"],
             ["Dossier", "/dossier"],
             ["Projects", "/projects"],
             ["Contact", "/contact"],
-          ].map(([title, url]) => (
-            <a href={url} class="font-display max-w-sm text-2xl font-bold leading-tight">
-              <span class="link link-underline link-underline-black text-black">
+          ].map(([title, url], index) => (
+            <a key={index} href={url} className="font-display max-w-sm text-2xl font-bold leading-tight">
+              <span className="link link-underline link-underline-black text-black">
                 {title}
               </span>
             </a>
@@ -24,6 +24,6 @@ export default function Header() {
       </div>
     </nav>
   );
-  
-  
+
+
 }
