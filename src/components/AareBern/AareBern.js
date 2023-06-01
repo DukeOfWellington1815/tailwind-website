56f05723060a01c2baf525c809b7caf3c0753eb3
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-06-01 20:59:23 +0200

    aareBern finished
    hehehee

--- src/components/AareBern/AareBern.js
+++ src/components/AareBern/AareBern.js
@@ -2,6 +2,9 @@ import { getAllTemps, getTempByCity, getTempInBern } from '../../middleware/apiA
 import { useEffect, useState } from "react"
 import './AareBern.css'
 
+import kanalOffen from '../../assets/images/kanal-offen.svg';
+import kanalZu from '../../assets/images/kanal-zu.svg';
+
 export default function Aare() {
 
     const [temp, setTemp] = useState({});
@@ -20,40 +23,46 @@ export default function Aare() {
             }
         }
         loadTemps();
+
+        // Set up an interval to refresh the data every 60 seconds
+        const intervalId = setInterval(loadTemps, 60000 * 5);
+
+        // Clean up the interval when the component is unmounted
+        return () => clearInterval(intervalId);
     }, [])
 
     console.log(temp);
 
     var kanalbild = ""
+    var kanalstatus = ""
 
     if (temp.bueber && temp.bueber.state === "open") {
-        kanalbild = "../../assets/images/kanal-offen.svg"
+        kanalbild = kanalOffen;
+
     } else if (temp.bueber && temp.bueber.state === "closed") {
-        kanalbild = "../../assets/images/kanal-zu.svg"
+        kanalbild = kanalZu
+        kanalstatus = "zu"
+
     }
 
     return !temp ? null : (
         <div className="Aare">
-            <h1>Aare im Moment</h1>
-
             {temp.aare && temp.aare.location && (
                 <div className='gameDiv grid gap-4 grid-cols-4 p-10'>
                     <h1 className='aare-stadt'>{temp.aare.location}</h1>
                     <article className='aare-box-temperatur container mx-auto bg-gray-200 rounded-xl shadow border p-8'>
+                       <p className='aare-info temperatur'>{temp.aare.temperature}°</p>
 
-                        <span className='aare-info-temperatur forecasttemp'>{temp.aare.temperature_text}</span>
-                        <p className='aare-info temperatur'>{temp.aare.temperature}°</p>
-                        <span className='aare-info-temperatur'>Z Wasser i ca. 2 Stung:</span>
-                        <span className='aare-info-temperatur forecasttemp'>{temp.aare.forecast2h}°</span>
-
+ 
+                        <span className='aare-info-temperatur forecasttemp'>Z Wasser i ca. 2 Stung:</span>
+                        <span className='aare-info forecasttemp'>{temp.aare.forecast2h}°</span>
+                        <span className='aare-info-temperatur forecasttemp'>Parole: {temp.aare.temperature_text}</span>
                     </article>
 
-                    <article className='aare-box-wasser container mx-auto bg-gray-200 rounded-xl shadow border p-8'>
-                        <div className='wasser-top'>
-                            <span className='aare-info-wasser'>{temp.aare.flow} m3/s;</span>
+                    <article className='aare-box-temperatur container mx-auto bg-gray-200 rounded-xl shadow border p-8'>
+                            <span className='aare-info temperatur'>{temp.aare.flow} m3/s</span>
                             <img className='kanalbild' src={kanalbild} alt={kanalstatus}/>
-                        </div>
-                        <p className='aare-info-wasser'>Wassermängi: {temp.aare.flow_text}</p>
+                        <p className='aare-info-temperatur forecasttemp'>Wassermängi: {temp.aare.flow_text}</p>
                     </article>
                 </div>
             )}
