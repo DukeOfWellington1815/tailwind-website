f372775cf24e1cb27fb1ed0a269f16aa82d52bcd
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-07-21 14:58:16 +0200

    backend sends a token

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 5000;
+
+const SECRET_KEY = '288c2e4187c38a7132a0e7717a8d29b4d3d7614d7204416a9f18b7d765f538866cb41c4d20b18f96ba3e755ea2d14625';
 
 // Sample user data (Replace this with data from your database)
 const users = [
@@ -18,6 +21,22 @@ const users = [
 app.use(bodyParser.json());
 app.use(cors());
 
+function verifyToken(req, res, next) {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+}
+
 // Login route
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
@@ -26,11 +45,21 @@ app.post('/api/login', (req, res) => {
 
   if (user && bcrypt.compareSync(password, user.password)) {
     // Successful login
-    res.status(200).json({ message: 'Login successful' });
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '1h' });
+
+    // Send the token in the response
+    res.status(200).json({ message: 'Login successful', token });
   } else {
     // Invalid credentials
     res.status(401).json({ error: 'Invalid username or password' });
   }
+});
+
+app.get('/api/protected', verifyToken, (req, res) => {
+  // Only authorized users can access this route
+  res.status(200).json({ message: 'Protected route accessed successfully' });
 });
 
 app.listen(port, () => {
