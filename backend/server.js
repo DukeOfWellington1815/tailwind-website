a2e099e5fbe14f9058dd7f73c767ade5cb609887
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-07-25 17:41:27 +0200

    implemented database and connection

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,39 @@
-// server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser'); // Import the cookie-parser library
+const cookieParser = require('cookie-parser');
+const mysql = require('mysql2');
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const port = 5000;
 
-const SECRET_KEY = '288c2e4187c38a7132a0e7717a8d29b4d3d7614d7204416a9f18b7d765f538866cb41c4d20b18f96ba3e755ea2d14625';
+// Create a MySQL database connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
-// Sample user data (Replace this with data from your database)
-const users = [
-  {
-    id: 1,
-    username: 'admin',
-    password: '$2a$10$K8IRHTzzyKJW9FE2C5Yvo.bH2F8kC/G4JBJiwzKinCwZuSxSCfeL.', // "password"
-  },
-];
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 app.use(bodyParser.json());
-app.use(cookieParser()); // Use cookie-parser middleware
+app.use(cookieParser());
 app.use(cors());
 
 function verifyToken(req, res, next) {
-  const token = req.cookies.token; // Read token from cookie
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ error: 'Access denied' });
@@ -41,33 +48,53 @@ function verifyToken(req, res, next) {
   }
 }
 
-// Login route
+// Login route - Fetch users from the database
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
-  const user = users.find((user) => user.username === username);
+  // Query the database to get the user
+  const query = 'SELECT * FROM users WHERE username = ?';
+  connection.query(query, [username], (error, results) => {
+    if (error) {
+      console.error('Error fetching user from the database:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
 
-  if (user && bcrypt.compareSync(password, user.password)) {
-    // Successful login
+    const user = results[0]; // Get the first user (assuming username is unique)
 
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '48h' });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      // Successful login
 
-    // Set the token in a cookie with the name "token", expires in 48 hours
-    res.cookie('token', token, { httpOnly: true, maxAge: 48 * 3600000 }); // 48 hours in milliseconds
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '48h' });
 
+      // Set the token in a cookie with the name "token", expires in 48 hours
+      res.cookie('token', token, { httpOnly: true, maxAge: 48 * 3600000 });
 
-    // Send a success message
-    res.status(200).json({ message: 'Login successful', user: { username: user.username } });
-  } else {
-    // Invalid credentials
-    res.status(401).json({ error: 'Invalid username or password' });
-  }
+      // Send a success message
+      res.status(200).json({ message: 'Login successful', user: { username: user.username } });
+    } else {
+      // Invalid credentials
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  });
 });
 
+// Retrieve abstracts from the database and post as JSON in the protected route
 app.get('/api/protected', verifyToken, (req, res) => {
   // Only authorized users can access this route
-  res.status(200).json({ message: 'Protected route accessed successfully' });
+
+  // Query the database to get the abstracts
+  const query = 'SELECT title, body FROM abstracts';
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error fetching abstracts from the database:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    // Send the abstracts as JSON
+    res.status(200).json(results);
+  });
 });
 
 app.listen(port, () => {
