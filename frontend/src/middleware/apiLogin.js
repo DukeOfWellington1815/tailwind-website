844c738fbe43a94a8daba0adca4cb834b3736511
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-07-26 14:34:38 +0200

    added token in header

--- frontend/src/middleware/apiLogin.js
+++ frontend/src/middleware/apiLogin.js
@@ -1,31 +1,33 @@
-// middleware/apiLogin.js
-
-const URL = "http://localhost:5000"; // backend URL
+const URL = 'http://localhost:5000'; // backend URL
 
 export async function login({ username, password }) {
   const response = await fetch(`${URL}/api/login`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ username, password }),
   });
 
   if (!response.ok) {
-    throw new Error("Invalid username or password");
+    throw new Error('Invalid username or password');
   }
 
   const data = await response.json();
-  return data; // Assuming the response only contains user data, without the token property
+  return data; // Assuming the response contains both user data and the JWT token
 }
 
-export async function getAllAbstracts() {
-  const response = await fetch(`${URL}/api/protected`)
+export async function getAllAbstracts(token) {
+  const response = await fetch(`${URL}/api/protected`, {
+    headers: {
+      Authorization: `Bearer ${token}`, // Include the token in the request header
+    },
+  });
 
   if (!response.ok) {
-      return Promise.reject(response)
+    return Promise.reject(response);
   }
 
-  const data = await response.json()
-  return data
+  const data = await response.json();
+  return data;
 }
