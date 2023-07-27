93c03ee443ad48c61c9c8752016f5183ffc96ea4
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-07-27 14:07:41 +0200

    corrected typo

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-xl">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h2 className="text-2xl text-center font-bold mb-4">Contact Us</h2>
+          <h2 className="text-2xl text-center font-bold mb-4">Contact Me</h2>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
               Name
