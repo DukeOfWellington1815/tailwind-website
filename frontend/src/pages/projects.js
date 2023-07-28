33b82fb467a194b0a6eb457c687aa9e0b7aacf19
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-07-28 14:37:17 +0200

    Update projects.js

--- frontend/src/pages/projects.js
+++ frontend/src/pages/projects.js
@@ -7,7 +7,6 @@ export default function ProjectsPage() {
 
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <h1>WELCOME TO MY PROJECTS PAGE</h1>
       <ProjectAbstract />
     </div>
   );
