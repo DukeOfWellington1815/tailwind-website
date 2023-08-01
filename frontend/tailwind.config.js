2f334b4ee7051da84807e1a2046671defacf12f1
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-01 11:17:58 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    extend: {
+      colors: {
+        primary: '#07C7F2',
+        secondary: '#07DBF2',
+        tetriary: '#0CF2DB',
+        dark: '#253540',
+        bright: '#F2F2F2',
+      },
+    },
+  },
+  variants: {
     extend: {},
-    // colors: {
-    //   'primary' : '#07C7F2'
-      
-    // },
   },
   plugins: [],
-}
+};
