66c1341cd6dc7507628c3e6418b3c77ed44508f1
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-01 15:18:22 +0200

    footer responsive

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -9,31 +9,23 @@ export default function Footer() {
   };
 
   return (
-    <footer className="bg-dark-color text-white p-4 bottom-0 left-0 w-full">
-      <div className="flex justify-between items-center">
-        <div>
-          <span>&copy; {new Date().getFullYear()} Your Company</span>
-        </div>
-        <div>
-          <button
-            onClick={togglePopup}
-            className="mr-4 hover:underline focus:outline-none"
-          >
+    <footer className="footer-container">
+      <div className="footer-content bg-dark-color">
+        <span>&copy; {new Date().getFullYear()} Your Company</span>
+        <div className="links">
+          <button onClick={togglePopup} className="mr-4 hover:underline focus:outline-none">
             Impressum
           </button>
-          <a
-            href="mailto:lorenzo.florezfritschi@protonmail.com"
-            className="hover:underline focus:outline-none"
-          >
+          <a href="mailto:lorenzo.florezfritschi@protonmail.com" className="hover:underline focus:outline-none">
             lorenzo.florezfritschi@protonmail.com
           </a>
         </div>
       </div>
       {isPopupOpen && (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-opacity-80 bg-black flex justify-center items-center">
-          <div className="bg-white p-8 rounded shadow">
-            <h2 className="text-black text-xl font-semibold mb-4">Impressum</h2>
-            <p className="text-black">
+        <div className="footer-popup">
+          <div className="popup-content">
+            <h2 className="text-xl font-semibold mb-4">Impressum</h2>
+            <p>
               Contact me at:
               <br />
               Email: [Your Email Address]
@@ -42,12 +34,7 @@ export default function Footer() {
               <br />
               Website: [Your Personal Website or Portfolio]
             </p>
-            <button
-              onClick={togglePopup}
-              className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-            >
-              Close
-            </button>
+            <button onClick={togglePopup}>Close</button>
           </div>
         </div>
       )}
