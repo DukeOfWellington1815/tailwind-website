9ca8ffb523abb9d099cdd0b0e8cf3ef095a13c34
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-10 11:33:52 +0200

    carousel size reversed needs fixing

--- frontend/src/components/ProjectAbstract/ProjectAbstract.js
+++ frontend/src/components/ProjectAbstract/ProjectAbstract.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import abstractsData from '../../assets/texts/abstracts.json';
-import "./ProjectAbstract.css";
-import logo from '../../assets/images/logo192.png';
-import placeholder from '../../assets/images/placeholder.png';
-import kanal from '../../assets/images/kanal-offen.svg';
-import { getAllAbstracts } from '../../middleware/apiLogin';
-import useSession from '../../middleware/session';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import useSession from '../../middleware/session';
+import { getAllAbstracts } from '../../middleware/apiLogin';
+import placeholder from '../../assets/images/placeholder.png';
+import "./ProjectAbstract.css";
 
 const ProjectAbstract = () => {
   const [abstracts, setAbstracts] = useState([]);
@@ -19,12 +16,10 @@ const ProjectAbstract = () => {
     document.title = 'Projects';
   }, []);
 
-  // Assuming you are using the useSession hook to get the token
   const { token } = useSession();
 
   useEffect(() => {
     const loadAbstracts = async () => {
-      // Check if the token is available
       if (!token) {
         setLoading(false);
         return;
@@ -32,7 +27,7 @@ const ProjectAbstract = () => {
 
       try {
         const data = await getAllAbstracts(token);
-        setAbstracts(data); // Assuming the response is an array of abstracts
+        setAbstracts(data);
         setLoading(false);
       } catch (error) {
         setError("Failed to fetch abstracts. Please try again later.");
@@ -40,9 +35,9 @@ const ProjectAbstract = () => {
       }
     };
     loadAbstracts();
-  }, [token]); // Execute the effect whenever the token changes
+  }, [token]);
 
-  const placeholderImages = [placeholder, logo, logo, placeholder, placeholder]; // Placeholder images
+  const placeholderImages = [placeholder, placeholder, placeholder, placeholder, placeholder];
 
   const slickSettings = {
     dots: false,
@@ -51,9 +46,16 @@ const ProjectAbstract = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: "20%", // Adjust the padding to your preference
+    centerPadding: "20%",
     nextArrow: null,
-    prevArrow: null
+    prevArrow: null,
+    beforeChange: (currentSlide, nextSlide) => {
+      const imageContainers = document.querySelectorAll(".image-container img");
+      imageContainers.forEach((img, index) => {
+        img.classList.toggle("center-image", index === nextSlide);
+        img.classList.toggle("side-image", index !== nextSlide);
+      });
+    },
   };
 
   return (
@@ -67,28 +69,49 @@ const ProjectAbstract = () => {
         abstracts.map((abstract, index) => (
           <div key={index} className="carousel-item">
             <Slider {...slickSettings}>
-              {(abstract.images && abstract.images.length > 0
-                ? abstract.images
-                : placeholderImages
-              ).map((image, imgIndex) => (
-                <div
-                  key={imgIndex}
-                  className={`relative ${imgIndex === 2 ? 'center-image slick-current' : 'side-image'}`}
-                >
-                  <div className="image-container">
-                    <img
-                      src={image}
-                      alt={abstract.title}
-                      className="h-screen w-full object-cover rounded-md mb-4"
-                    />
-                  </div>
-                  <div className="absolute inset-0 bg-dark-color bg-opacity-40 rounded-md transition-opacity opacity-0 hover:opacity-100">
-                    <div className="flex items-center justify-center h-full">
-                      <button className="text-bright-color font-semibold">View Details</button>
+              {abstract.images && abstract.images.length > 0 ? (
+                // Render images if available
+                abstract.images.map((image, imgIndex) => (
+                  <div
+                    key={imgIndex}
+                    className={`relative center-image'}`}
+                  >
+                    <div className="image-container">
+                      <img
+                        src={image}
+                        alt={abstract.title}
+                        className="h-screen w-full object-cover rounded-md mb-4"
+                      />
+                    </div>
+                    <div className={`absolute inset-0 bg-dark-color bg-opacity-40 rounded-md transition-opacity ${imgIndex === 2 ? 'opacity-100' : 'opacity-0'} hover:opacity-100`}>
+                      <div className="flex items-center justify-center h-full">
+                        <button className="text-bright-color font-semibold">View Details</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))
+              ) : (
+                // Render placeholders if no images available
+                placeholderImages.map((image, imgIndex) => (
+                  <div
+                    key={imgIndex}
+                    className={`relative center-image'}`}
+                  >
+                    <div className="image-container">
+                      <img
+                        src={image}
+                        alt="Placeholder"
+                        className="h-screen w-full object-cover rounded-md mb-4"
+                      />
+                    </div>
+                    <div className={`absolute inset-0 bg-dark-color bg-opacity-40 rounded-md transition-opacity hover:opacity-100`}>
+                      <div className="flex items-center justify-center h-full">
+                        <button className="text-bright-color font-semibold">View Details</button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              )}
             </Slider>
             <h2 className="text-lg font-semibold mb-2 primary-color">{abstract.title}</h2>
             <p className="text-gray-700">{abstract.body}</p>
