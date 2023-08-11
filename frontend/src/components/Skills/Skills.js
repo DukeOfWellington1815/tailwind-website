dbcca10d96cd9f759b6c754a8bb7e805f0503918
FatMonkee <89774607+FatMonkee@users.noreply.github.com>
2023-08-11 14:46:03 +0200

    Update Skills.js

--- frontend/src/components/Skills/Skills.js
+++ frontend/src/components/Skills/Skills.js
@@ -7,10 +7,12 @@ const skills = SkillText;
 const grades = GradesText;
 
 export default function SkillsPage() {
-  const [visibleIndexes, setVisibleIndexes] = useState([]);
+  const [visibleSkillIndexes, setVisibleSkillIndexes] = useState([]);
+  const [visibleGradeIndexes, setVisibleGradeIndexes] = useState([]);
 
   useEffect(() => {
     const handleScroll = () => {
+      // Calculate visible skill indexes based on scroll position
       const skillsSection = document.getElementById('skills-section');
       if (skillsSection) {
         const skillsTop = skillsSection.getBoundingClientRect().top;
@@ -21,7 +23,21 @@ export default function SkillsPage() {
         );
 
         const newIndexes = Array.from({ length: currentIndex + 1 }, (_, index) => index);
-        setVisibleIndexes(newIndexes);
+        setVisibleSkillIndexes(newIndexes);
+      }
+
+      // Calculate visible grade indexes based on scroll position
+      const gradesSection = document.getElementById('grades-section');
+      if (gradesSection) {
+        const gradesTop = gradesSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        const currentIndex = Math.min(
+          Math.floor((windowHeight * 0.75 - gradesTop) / 100),
+          grades.length - 1
+        );
+
+        const newIndexes = Array.from({ length: currentIndex + 1 }, (_, index) => index);
+        setVisibleGradeIndexes(newIndexes);
       }
     };
 
@@ -53,67 +69,70 @@ export default function SkillsPage() {
     return `${width}%`;
   };
 
-return (
-  <div className="flex items-center justify-center">
-    <div className="flex space-x-64">
-      <div id='skills-section' className="bright-color p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold text-blue-500 mb-4">My Skills</h2>
-        <div className="space-y-4">
-          {skills.map((skillCategory, index) => (
-            <div key={skillCategory.category}>
-              <h3 className="text-lg font-semibold mb-2">{skillCategory.category}</h3>
-              <ul className="space-y-2">
-                {skillCategory.list.map((skill, skillIndex) => (
-                  <li
-                    key={skill.name}
-                    className={`${
-                      visibleIndexes.includes(skillIndex) ? 'fade-in' : 'hidden'
-                    } transition-opacity duration-500 ease-in`}
-                    style={{ animationDelay: `${skillIndex * 0.2}s` }}
-                  >
-                    <div className="flex items-center justify-between">
-                      <span>{skill.name}</span>
-                      <div className="flex items-center space-x-2">
-                        <span>{skill.level}%</span>
-                        <div className="h-2 w-32 bg-gray-800 rounded-full">
-                          <div
-                            className="h-2 bg-blue-500   rounded-full"
-                            style={{ width: `${skill.level}%` }}
-                          ></div>
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex space-x-64">
+        <div id='skills-section' className="bright-color p-8 rounded shadow-md w-96">
+          <h2 className="text-2xl font-bold text-blue-500 mb-4">My Skills</h2>
+          <div className="space-y-4">
+            {skills.map((skillCategory, index) => (
+              <div key={skillCategory.category}>
+                <h3 className="text-lg font-semibold mb-2">{skillCategory.category}</h3>
+                <ul className="space-y-2">
+                  {skillCategory.list.map((skill, skillIndex) => (
+                    <li
+                      key={skill.name}
+                      className={`${visibleSkillIndexes.includes(skillIndex) ? 'fade-in' : 'hidden'
+                        } transition-opacity duration-500 ease-in`}
+                      style={{ animationDelay: `${skillIndex * 0.2}s` }}
+                    >
+                      <div className="flex items-center justify-between">
+                        <span>{skill.name}</span>
+                        <div className="flex items-center space-x-2">
+                          <span>{skill.level}%</span>
+                          <div className="h-2 w-32 bg-gray-800 rounded-full">
+                            <div
+                              className="h-2 bg-blue-500   rounded-full"
+                              style={{ width: `${skill.level}%` }}
+                            ></div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="bright-color p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-2 fade-in-color">My Grades</h2>
-        <ul className="space-y-2">
-        <h3 className="text-lg font-semibold mb-2">BWD Bern</h3>
-          {grades.map((grade, gradeIndex) => (
-            <li key={gradeIndex}>
-              <div className="flex items-center justify-between">
-                <span>{grade.subject}</span>
-                <div className="flex items-center space-x-2">
-                  <span>{grade.grade}</span>
-                  <div className="h-2 w-32 bg-gray-800 rounded-full">
-                    <div
-                      className="h-2 progress-filling rounded-full"
-                      style={{ width: gradeToProgressBarWidth(grade.grade) }}
-                    ></div>
+        <div id='grades-section' className="bright-color p-8 rounded shadow-md w-96">
+          <h2 className="text-2xl font-bold mb-2 fade-in-color">My Grades</h2>
+          <ul className="space-y-2">
+            <h3 className="text-lg font-semibold mb-2">BWD Bern</h3>
+            {grades.map((grade, gradeIndex) => (
+              <li
+                key={gradeIndex}
+                className={`${visibleGradeIndexes.includes(gradeIndex) ? 'fade-in' : 'hidden'
+                  } transition-opacity duration-500 ease-in`}
+                style={{ animationDelay: `${gradeIndex * 0.2}s` }}
+              >
+                <div className="flex items-center justify-between">
+                  <span>{grade.subject}</span>
+                  <div className="flex items-center space-x-2">
+                    <span>{grade.grade}</span>
+                    <div className="h-2 w-32 bg-gray-800 rounded-full">
+                      <div
+                        className="h-2 progress-filling rounded-full"
+                        style={{ width: gradeToProgressBarWidth(grade.grade) }}
+                      ></div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
 }
