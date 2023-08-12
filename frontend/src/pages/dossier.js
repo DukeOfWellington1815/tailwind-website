e47f8d0616d13c45e432e99d161945767e029680
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-12 12:47:33 +0200

    Update dossier.js

--- frontend/src/pages/dossier.js
+++ frontend/src/pages/dossier.js
@@ -1,13 +1,89 @@
-import Header from "../layouts/Header"
-import AareBern from "../components/AareBern/AareBern"
-import GameLib from "../components/GameLib/GameLib"
-import CV_EN from "../assets/docs/CV_EN.pdf"
+import React, { useState, useRef, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import CV_EN from "../assets/docs/CV_EN.pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css"; // Import the required CSS
 
-export default function dossier() {
-    return (
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <h1>WELCOME TO MY DOSSIER PAGE</h1>
-        <embed src={CV_EN} width="800" height="1200" type="application/pdf"></embed>
-      </div>
-    );
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function Dossier() {
+  const [numPages, setNumPages] = useState(null);
+  const [containerWidth, setContainerWidth] = useState(null);
+  const containerRef = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+  const [zoomLevel, setZoomLevel] = useState(1.0);
+
+  function handleDownload() {
+    const link = document.createElement("a");
+    link.href = CV_EN;
+    link.target = "_blank";
+    link.download = "CV_EN.pdf";
+    link.click();
   }
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setIsLoading(false);
+  }
+
+  function handleZoomIn() {
+    setZoomLevel(prevZoom => prevZoom + 0.2);
+  }
+
+  function handleZoomOut() {
+    setZoomLevel(prevZoom => Math.max(0.2, prevZoom - 0.2));
+  }
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const width = containerRef.current.offsetWidth;
+      setContainerWidth(width * zoomLevel);
+
+      // Scroll to keep the page centered after zoom
+      const container = containerRef.current;
+      const scrollLeft = (container.scrollWidth - container.offsetWidth) / 2;
+      container.scrollLeft = scrollLeft;
+    }
+  }, [zoomLevel]);
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-center mt-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2"
+          onClick={handleZoomIn}
+        >
+          Zoom In
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleZoomOut}
+        >
+          Zoom Out
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+          onClick={handleDownload}
+        >
+          Download PDF
+        </button>
+      </div>
+      <div className="p-8 w-full md:w-3/4 lg:w-1/2 xl:w-1/3 flex justify-center items-center" ref={containerRef}>
+        <Document file={CV_EN} onLoadSuccess={onDocumentLoadSuccess} loading="">
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page
+              className="shadow-lg mb-4"
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={containerWidth}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+              loading=""
+            />
+          ))}
+        </Document>
+      </div>
+    </div>
+  );
+}
+
+export default Dossier;
