705f4d87042844d697d55f93a79f14f956851b90
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-13 17:18:52 +0200

    home responsive

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -26,9 +26,9 @@ export default function HomePage() {
 
   return (
     <div className='flex flex-col items-center overflow-hidden'>
-      <div className='welcome-section text-7xl font-bold mt-5 bright-color max-w-7xl'>
+      <div className='welcome-section text-4xl md:text-7xl font-bold mt-5 bright-color max-w-7xl'>
         {typedMessage}
-        {showArrow && <div className={`bright-color text-4xl mt-32 animate-bounce`}>&#8595;</div>}
+        {showArrow && <div className={`bright-color text-4xl mt-10 md:mt-32 animate-bounce`}>&#8595;</div>}
       </div>
 
       <div className='my-5 bright-color'>
