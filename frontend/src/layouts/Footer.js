9823aa35ddf4f148f460ca842c9955332b4fe6e3
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-13 18:51:59 +0200

    Update Footer.js

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -10,13 +10,13 @@ export default function Footer() {
 
   return (
     <footer className="footer-container">
-      <div className="footer-content bg-dark-color">
+      <div className="footer-content">
         <span>&copy; {new Date().getFullYear()} Lorenzo Flórez Fritschi</span>
         <div className="links">
           <button onClick={togglePopup} className="mr-4 hover:underline focus:outline-none">
             Imprint
           </button>
-          <a href="mailto:lorenzo.florezfritschi@protonmail.com" className="hover:underline focus:outline-none">
+          <a href="mailto:lorenzo.florez.fritschi@protonmail.com" className="hover:underline focus:outline-none">
             lorenzo.florez.fritschi@protonmail.com
           </a>
         </div>
@@ -28,11 +28,12 @@ export default function Footer() {
             <p>
               Contact me at:
               <br />
-              Email: lorenzo.florez.fritschi@protonmail.com
+              <br />
+              Email: <a href="mailto:lorenzo.florez.fritschi@protonmail.com" className="hover:underline focus:outline-none">lorenzo.florez.fritschi@protonmail.com</a>
               <br />
               Phone: +41 764411255
               <br />
-              Website: fatmonkee.com
+              Website: <a href="https://fatmonkee.com" className="hover:underline focus:outline-none">fatmonkee.com</a>
             </p>
             <button onClick={togglePopup}>Close</button>
           </div>
