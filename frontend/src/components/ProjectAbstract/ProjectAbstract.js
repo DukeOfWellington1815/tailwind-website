4ce33385dca412efc13fcca72385289d0af72e0d
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-13 17:12:41 +0200

    projectabstracts now responsive

--- frontend/src/components/ProjectAbstract/ProjectAbstract.js
+++ frontend/src/components/ProjectAbstract/ProjectAbstract.js
@@ -38,49 +38,11 @@ import imagesc4 from "../../assets/images/sc/image4.png";
 import imagesc5 from "../../assets/images/sc/image5.png";
 
 export default function ProjectAbstract() {
-  const [abstracts, setAbstracts] = useState([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [abstracts, setAbstracts] = useState(projectData); // Use local data
   const [visibleIndexes, setVisibleIndexes] = useState([]);
-
-  useEffect(() => {
-    document.title = 'Projects';
-  }, []);
-
-  const { token } = useSession();
-
-  useEffect(() => {
-    document.title = 'Projects';
-
-    if (token) {
-      const loadAbstracts = async () => {
-        try {
-          const data = await getAllAbstracts(token);
-
-          if (data.length === 0) {
-            setAbstracts(projectData); // Use local data if API response is empty
-          } else {
-            const parsedData = data.map(item => ({
-              ...item,
-              imagepaths: JSON.parse(item.imagepaths),
-              collaborators: JSON.parse(item.collaborators)
-            }));
-
-            setAbstracts(parsedData);
-          }
-          setLoading(false);
-        } catch (error) {
-          setError("Failed to fetch abstracts. Please try again later.");
-          setLoading(false);
-        }
-      };
-      loadAbstracts();
-    } else {
-      setAbstracts(projectData); // Use local data if no token
-      setLoading(false);
-    }
-  }, [token]);
-
+  const [loading, setLoading] = useState(false); // Add this line
+  const [error, setError] = useState(""); // Add this line
+  
   const placeholderImages = [placeholder, placeholder, placeholder, placeholder, placeholder];
 
   const slickSettings = {
@@ -146,101 +108,102 @@ export default function ProjectAbstract() {
 
   return (
     <div className="carousel-container">
-      {loading ? (
-        <p>Loading abstracts...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        abstracts.map((abstract, index) => {
-          const isVisible = visibleIndexes.includes(index);
-          const imagesForProject = projectImages[abstract.title] || placeholderImages;
+  {loading ? (
+    <p>Loading abstracts...</p>
+  ) : error ? (
+    <p>{error}</p>
+  ) : (
+    abstracts.map((abstract, index) => {
+      const isVisible = visibleIndexes.includes(index);
+      const imagesForProject = projectImages[abstract.title] || placeholderImages;
 
-          return (
-            <div
-              key={index}
-              className={`carousel-item my-64 ${isVisible ? 'fade-in' : ''}`}
-              style={{ animationDelay: `${index * 0.2}s` }}
-            >
-              <div className={`text-7xl font-bold mt-16 mb-4 bright-color uppercase mx-32 content ${isVisible ? 'visible' : ''}`}>
-                <h2>{abstract.title}– <br />{abstract.slogan}</h2>
-                <hr />
+      return (
+        <div
+          key={index}
+          className={`carousel-item my-12 md:my-64 ${isVisible ? 'fade-in' : ''}`}
+          style={{ animationDelay: `${index * 0.2}s` }}
+        >
+          <div className={`text-3xl md:text-7xl font-bold mt-8 md:mt-16 mb-2 md:mb-4 bright-color uppercase mx-4 md:mx-32 content ${isVisible ? 'visible' : ''}`}>
+            <h2>{abstract.title}– <br className="md:hidden" />{abstract.slogan}</h2>
+            <hr />
+          </div>
+
+          <Slider {...slickSettings}>
+            {imagesForProject.map((image, imgIndex) => (
+              <div key={imgIndex} className="relative image-container">
+                <img
+                  src={image}
+                  alt={abstract.title}
+                  className={`h-64 md:h-screen w-full object-contain rounded-md mx-auto content ${isVisible ? ' visible' : ''}`}
+                />
               </div>
+            ))}
+          </Slider>
 
-              <Slider {...slickSettings}>
-                {imagesForProject.map((image, imgIndex) => (
-                  <div key={imgIndex} className="relative image-container">
-                    <img
-                      src={image}
-                      alt={abstract.title}
-                      className={`h-screen w-full object-contain rounded-md mx-auto content ${isVisible ? ' visible' : ''}`}
-                    />
-                  </div>
-                ))}
-              </Slider>
-
-              <div className={`flex flex-col items-center mx-64 mt-10 content ${isVisible ? 'visible' : ''}`}>
-                <div className="flex flex-row">
-                  <div className="flex-1/3 text-xl max-w-md">
-                    <div className="flex flex-col">
-                      <div className="w-full">
-                        <table className="table bright-color text-2xl">
-                          <tbody>
-                            <tr className="flex">
-                              <td className="pr-2">Year:</td>
-                              <td className="tetriary-color">{abstract.year}</td>
-                            </tr>
-                            <tr className="flex">
-                              <td className="pr-2">Type:</td>
-                              <td className="primary-color">{abstract.type}</td>
-                            </tr>
-                            <tr className="flex">
-                              <td className="pr-2">Role:</td>
-                              <td className="secondary-color">{abstract.own_role}</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex-2/3 max-w-4xl ml-60">
-                    <p className={`bright-color text-4xl ${isVisible ? 'fade-in visible' : ''}`}>{abstract.body}</p>
-                    <div className={`flex items-center justify-end mt-4 ${isVisible ? 'fade-in visible' : ''}`}>
-                      <a href={`/abstract/${abstract.id}`} className={`dark-color bg-primary-color px-4 py-2 rounded-md hover:opacity-80 ${isVisible ? 'fade-in visible' : ''}`}>
-                        Read More
-                      </a>
-                    </div>
-                    <div className={`mt-10 ${isVisible ? 'fade-in visible' : ''}`}>
-                      <p className={`bright-color text-md ${isVisible ? 'fade-in visible' : ''}`}>
-                        {abstract.collaborators &&
-                          abstract.collaborators.map((roleWithPeople, collaboratorIndex) => (
-                            <span key={collaboratorIndex}>
-                              <span className="text-gray-500 uppercase font-semibold">{roleWithPeople.role}:</span>{" "}
-                              {roleWithPeople.people.map((collaborator, personIndex) => (
-                                <React.Fragment key={personIndex}>
-                                  <a
-                                    href={collaborator.website_url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="uppercase"
-                                  >
-                                    {collaborator.name}
-                                  </a>
-                                  {personIndex !== roleWithPeople.people.length - 1 && ", "}
-                                </React.Fragment>
-                              ))}
-                              {collaboratorIndex !== abstract.collaborators.length - 1 && " "}
-                            </span>
-                          ))}
-                      </p>
-                    </div>
+          <div className={`flex flex-col items-center mx-4 md:mx-64 mt-4 md:mt-10 content ${isVisible ? 'visible' : ''}`}>
+            <div className="flex flex-col md:flex-row">
+              <div className="md:flex-1/3 text-base md:text-xl max-w-md">
+                <div className="flex flex-col">
+                  <div className="w-full">
+                    <table className="table bright-color text-2xl">
+                      <tbody>
+                        <tr className="flex">
+                          <td className="pr-2">Year:</td>
+                          <td className="tetriary-color">{abstract.year}</td>
+                        </tr>
+                        <tr className="flex">
+                          <td className="pr-2">Type:</td>
+                          <td className="primary-color">{abstract.type}</td>
+                        </tr>
+                        <tr className="flex">
+                          <td className="pr-2">Role:</td>
+                          <td className="secondary-color">{abstract.own_role}</td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
+              <div className="md:flex-2/3 max-w-full md:max-w-4xl md:ml-16">
+                <p className={`bright-color text-4xl ${isVisible ? 'fade-in visible' : ''}`}>{abstract.body}</p>
+                <div className={`flex items-center justify-end mt-4 ${isVisible ? 'fade-in visible' : ''}`}>
+                  <a href={`/abstract/${abstract.id}`} className={`dark-color bg-primary-color px-4 py-2 rounded-md hover:opacity-80 ${isVisible ? 'fade-in visible' : ''}`}>
+                    Read More
+                  </a>
+                </div>
+                <div className={`mt-10 ${isVisible ? 'fade-in visible' : ''}`}>
+                  <p className={`bright-color text-md ${isVisible ? 'fade-in visible' : ''}`}>
+                    {abstract.collaborators &&
+                      abstract.collaborators.map((roleWithPeople, collaboratorIndex) => (
+                        <span key={collaboratorIndex}>
+                          <span className="text-gray-500 uppercase font-semibold">{roleWithPeople.role}:</span>{" "}
+                          {roleWithPeople.people.map((collaborator, personIndex) => (
+                            <React.Fragment key={personIndex}>
+                              <a
+                                href={collaborator.website_url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="uppercase"
+                              >
+                                {collaborator.name}
+                              </a>
+                              {personIndex !== roleWithPeople.people.length - 1 && ", "}
+                            </React.Fragment>
+                          ))}
+                          {collaboratorIndex !== abstract.collaborators.length - 1 && " "}
+                        </span>
+                      ))}
+                  </p>
+                </div>
+              </div>
             </div>
-          );
-        })
-      )}
-    </div>
+          </div>
+        </div>
+      );
+    })
+  )}
+</div>
+
   );
 
 }
