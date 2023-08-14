5c6de10c1d236d935866de6f7357fa99f67f7986
DukeOfWellington1815 <115478238+DukeOfWellington1815@users.noreply.github.com>
2023-08-14 19:29:42 +0200

    working php

---
+++ backend/php/index.php
@@ -0,0 +1,75 @@
+<?php
+require 'vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$SECRET_KEY = $_ENV['SECRET_KEY'];
+$mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+
+if ($mysqli->connect_error) {
+    die('Database connection failed: ' . $mysqli->connect_error);
+}
+
+function verifyToken($token) {
+    global $SECRET_KEY;
+
+    try {
+        $decoded = JWT::decode($token, $SECRET_KEY, array('HS256'));
+        return $decoded->userId;
+    } catch (Exception $e) {
+        return false;
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/api/login') {
+    if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+        http_response_code(400);
+        echo json_encode(array('error' => 'Invalid Content-Type header'));
+        exit;
+    }
+
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    $username = isset($requestData['username']) ? trim($requestData['username']) : '';
+    $password = isset($requestData['password']) ? $requestData['password'] : '';
+
+    if (!empty($username) && !empty($password)) {
+        $username = $mysqli->real_escape_string($username);
+
+        $query = 'SELECT * FROM users WHERE username = ?';
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+        $stmt->close();
+
+        if ($user && password_verify($password, $user['password'])) {
+            $tokenPayload = array(
+                'userId' => $user['id'],
+            );
+
+            $token = JWT::encode($tokenPayload, $SECRET_KEY, 'HS256');
+
+            $response = array(
+                'token' => $token,
+                'message' => 'Login successful',
+                'user' => array('username' => $user['username'])
+            );
+            echo json_encode($response);
+        } else {
+            http_response_code(401);
+            echo json_encode(array('error' => 'Invalid username or password'));
+        }
+    } else {
+        http_response_code(400);
+        echo json_encode(array('error' => 'Username and password are required'));
+    }
+}
+
+$mysqli->close();
+?>
